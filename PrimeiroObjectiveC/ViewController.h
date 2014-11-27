945299c98f35d67493514124f9939f9421a50625
Nuno Cardoso <nunocardoso@nunos-mac-pro.home>
2014-11-27 17:29:18 +0000

    Initial Commit

---
+++ PrimeiroObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrimeiroObjectiveC
+//
+//  Created by Nuno Cardoso on 27/11/14.
+//  Copyright (c) 2014 Nuno Cardoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PrimeiroObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PrimeiroObjectiveC
+//
+//  Created by Nuno Cardoso on 27/11/14.
+//  Copyright (c) 2014 Nuno Cardoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
